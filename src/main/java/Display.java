656e5812d28f1318cbcb4628e77f072d9f4a345e
Layth Mehdi <layth.mehdi@kcl.ac.uk>
2019-11-15 13:59:51 +0000

    Store photo of board on save, code refactor

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,17 +1,27 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.util.Properties;
 
 public class Display {
 
   private static final int WIDTH = 900;
   private static final int HEIGHT = 600;
 
+  private int panelNumber = 1;
+
   private JFrame mainFrame;
   private JPanel mainPanel;
   private JPanel boardPanel;
   private JTextField boardTitle;
   private JButton newButton;
+
+  private JMenuItem exit;
+  private JMenuItem newBoard;
+  private JMenuItem save;
 
   public Display() {
 
@@ -24,12 +34,9 @@ public class Display {
     mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     mainFrame.setVisible(true);
 
-
     makeButton();
     makeMainPanel();
     makeMenuBar();
-    // makeTextArea();
-
  }
 
   public void makeMainPanel() {
@@ -59,6 +66,7 @@ public class Display {
 
     boardPanel = new JPanel(new BorderLayout());
     boardTitle = new JTextField();
+    boardTitle.setText("");
 
     boardTitle.setFont(font1);
     boardTitle.setHorizontalAlignment(JTextField.CENTER);
@@ -78,17 +86,30 @@ public class Display {
     JMenu file = new JMenu("File");
     menuBar.add(file);
 
-    JMenuItem exit = new JMenuItem("Exit");
+    JMenu makeNew = new JMenu("New");
+    menuBar.add(makeNew);
+
+    exit = new JMenuItem("Exit");
+    newBoard = new JMenuItem("New Kanban Board");
+    save = new JMenuItem("Save");
+
+    assignActions();
+    makeShortcuts();
+
+    file.add(exit);
+    file.add(save);
+    makeNew.add(newBoard);
+
+    mainFrame.setJMenuBar(menuBar);
+  }
+
+  public void assignActions() {
     exit.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent ev) {
           System.exit(0);
         }
-      });
+    });
 
-    KeyStroke exitShortcut = KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.CTRL_DOWN_MASK);
-    exit.setAccelerator(exitShortcut);
-
-    JMenuItem newBoard = new JMenuItem("New Kanban Board");
     newBoard.addActionListener(new ActionListener () {
       public void actionPerformed(ActionEvent ev) {
         String input = JOptionPane.showInputDialog("Input name of Kanban Board");
@@ -100,12 +121,53 @@ public class Display {
       }
     });
 
+
+    save.addActionListener(new ActionListener () {
+      public void actionPerformed(ActionEvent ev) {
+        String fileName = "temp.csv";
+        try {
+          FileWriter fileWriter = new FileWriter(fileName, true);
+
+          BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+          bufferedWriter.write(boardTitle.getText());
+          bufferedWriter.newLine();
+          bufferedWriter.close();
+        }
+        catch(IOException ex){
+          System.out.println("Error writing to file '" + fileName + "'");
+        }
+
+        takePicture(boardPanel);
+        panelNumber++;
+
+      }
+    });
+
+  }
+
+  public void makeShortcuts() {
+    KeyStroke exitShortcut = KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.CTRL_DOWN_MASK);
+    exit.setAccelerator(exitShortcut);
+
     KeyStroke newBoardShortcut = KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK);
     newBoard.setAccelerator(newBoardShortcut);
 
-    file.add(exit);
-    file.add(newBoard);
-    mainFrame.setJMenuBar(menuBar);
+    KeyStroke saveShortcut = KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK);
+    save.setAccelerator(saveShortcut);
+
+  }
+
+  // code sourced from https://stackoverflow.com/questions/18806287/how-to-save-jpanel-as-jpeg-with-its-components
+  public void takePicture(JPanel panel) {
+      BufferedImage img = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+      panel.print(img.getGraphics()); // or: panel.printAll(...);
+      try {
+          ImageIO.write(img, "jpg", new File("Kanban-Board-" + panelNumber + ".jpg"));
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
   }
 
 }
