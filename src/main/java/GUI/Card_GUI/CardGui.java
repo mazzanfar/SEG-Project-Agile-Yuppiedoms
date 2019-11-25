e522c47c80c05a147328940d5d583f39094bc992
Ziyang Chen <ziyang.2.chen@kcl.ac.uk>
2019-11-25 17:11:53 +0000

    added some fixing and styling

--- src/main/java/GUI/Card_GUI/CardGui.java
+++ src/main/java/GUI/Card_GUI/CardGui.java
@@ -2,32 +2,35 @@ package GUI.Card_GUI;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import Business_Logic.Card;
-//import javax.imageio.ImageIO;
-//import javax.smartcardio.Card;
 public class CardGui extends JFrame implements ActionListener{
     private JPanel titlePanel;
     private JPanel descriptionPanel;
-    private JPanel buttonPane;
+    private JPanel buttonPanel;
+    private JPanel IdPanel;
+    private JPanel SpPanel;
+
     private JButton changeButton;
     private JButton saveButton;
+
     private JTextField title;
     private JTextField description;
-    private JPanel SpPanel;
+
     private JComboBox<String> SpDropDown;
+
     private JLabel ID;
     private JLabel descriptionLabel;
     private JLabel titleLabel;
+    private JLabel IdLabel;
+    private JLabel SpLabel;
+
     private Card card;
-
-    private Card temp;
-
     public CardGui(Card inputCard) {
         card = inputCard;
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(900, 600));
         this.setMinimumSize(new Dimension(900, 600));
         this.setVisible(true);
@@ -41,9 +44,9 @@ public class CardGui extends JFrame implements ActionListener{
     }
 
     public void makeButton() {
-        buttonPane = new JPanel(new FlowLayout());
-        buttonPane.setVisible(true);
-        buttonPane.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.setVisible(true);
+        //buttonPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
         changeButton = new JButton();
         changeButton.setRolloverEnabled(true);
@@ -57,26 +60,26 @@ public class CardGui extends JFrame implements ActionListener{
         saveButton.setActionCommand("save");
         saveButton.addActionListener(this);
 
-        buttonPane.add(changeButton);
-        buttonPane.add(saveButton);
-        this.add(buttonPane,BorderLayout.SOUTH);
+        buttonPanel.add(changeButton);
+        buttonPanel.add(saveButton);
+        this.add(buttonPanel,BorderLayout.SOUTH);
     }
 
     public void  makeTitle() {
         titleLabel = new JLabel("Title: ");
+        titleLabel.setFont(new Font("Courier New", Font.ITALIC, 24));
         titlePanel = new JPanel(new BorderLayout());
         titlePanel.setVisible(true);
-        titlePanel.setBorder(BorderFactory.createLineBorder(Color.red));
 
         title = new JTextField(5);
         title.setVisible(true);
         title.setText(card.getTitle());
         title.setEditable(false);
 
-        //add actions
-
         titlePanel.add(title, BorderLayout.CENTER);
         titlePanel.add(titleLabel, BorderLayout.NORTH);
+        titlePanel.setBorder(new EmptyBorder(0, 43, 0, 112));
+//        titlePanel.setBorder(BorderFactory.createLineBorder(Color.red));
         this.add(titlePanel, BorderLayout.NORTH);
     }
 
@@ -101,14 +104,25 @@ public class CardGui extends JFrame implements ActionListener{
             description.setEditable(false);
             card.setDescription(description.getText());
             card.setTitle(title.getText());
+            card.setStoryPoints(SpDropDown.getToolTipText());
+            this.dispose();
         }
     }
 
     public void makeID()
     {
-        int temp = card.getID();
-        ID = new JLabel("temp");
-        this.add(ID, BorderLayout.WEST);
+        IdPanel = new JPanel(new GridLayout(1,2,0,0));
+        IdLabel = new JLabel("ID:");
+
+        ID = new JLabel(Integer.toString(card.getID()));
+        IdLabel.setFont(new Font("Courier New", Font.ITALIC, 12));
+        ID.setFont(new Font("Courier New", Font.ITALIC, 12));
+
+
+        IdPanel.add(IdLabel);
+        IdPanel.add(ID);
+        IdPanel.setBorder(new EmptyBorder(450, 0, 0, 0));
+        this.add(IdPanel, BorderLayout.WEST);
         //TODO: make jlabel ID by getting the id from Card class.
     }
 
@@ -116,18 +130,26 @@ public class CardGui extends JFrame implements ActionListener{
     {
         String[] choices = { "1", "2", "3", "4", "5"};
 
+        SpLabel = new JLabel("StoryPoint:");
+        SpLabel.setFont(new Font("Courier New", Font.ITALIC, 12));
         SpDropDown = new JComboBox<String>(choices);
-        SpPanel = new JPanel(new BorderLayout());
-        SpPanel.add(SpDropDown, BorderLayout.CENTER);
+        SpDropDown.setSelectedItem(card.getStoryPoints());
+        SpDropDown.setFont(new Font("Courier New", Font.ITALIC, 12));
+        SpDropDown.setMaximumSize(new Dimension(0,10));
+        SpPanel = new JPanel(new FlowLayout(1,0,0));
+        SpPanel.add(SpLabel);
+        SpPanel.add(SpDropDown);
+        SpPanel.setBorder(new EmptyBorder(450, 0, 0, 0));
         this.add(SpPanel, BorderLayout.EAST);
         SpDropDown.setVisible(true);
     }
 
     public void makeDescription() {
-        
+
         descriptionLabel = new JLabel("Description: ");
+        descriptionLabel.setFont(new Font("Courier New", Font.ITALIC, 24));
         descriptionPanel = new JPanel(new BorderLayout());
-        descriptionPanel.setBorder(BorderFactory.createLineBorder(Color.green));
+        //descriptionPanel.setBorder(BorderFactory.createLineBorder(Color.green));
 
         description = new JTextField(20);
         description.setText(card.getDescription());
@@ -141,7 +163,8 @@ public class CardGui extends JFrame implements ActionListener{
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
             public void run(){
-                new CardGui(new Card("11","dd","1111"));
+                new CardGui(new Card("11","dd","3"));
+                new CardGui(new Card("11","dd","4"));
                 //just for testing.
                 //remove later.
             }
