de6689041596e8094e1028888a0651e00171813f
WilliamZard <william.zard@kcl.ac.uk>
2019-12-05 21:50:15 +0000

    Add drag pane and drop pane for the DnD

---
+++ src/main/java/GUI/Column_GUI/DragPane.java
@@ -0,0 +1,32 @@
+package GUI.Column_GUI;
+
+import Business_Logic.Card;
+import Business_Logic.Column;
+import GUI.Card_GUI.*;
+import GUI.Transfer.DragGestureHandler;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureRecognizer;
+import java.awt.dnd.DragSource;
+import javax.swing.JPanel;
+
+public class DragPane extends JPanel {
+
+    private DragGestureRecognizer dgr;
+    private DragGestureHandler dragGestureHandler;
+
+    public DragPane() {
+        System.out.println("DragPane = " + this.hashCode());
+        setBackground(Color.RED);
+        dragGestureHandler = new DragGestureHandler(this);
+        dgr = DragSource.getDefaultDragSource().createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_MOVE, dragGestureHandler);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(50, 50);
+    }
+
+}
---
+++ src/main/java/GUI/Column_GUI/DropPane.java
@@ -0,0 +1,37 @@
+package GUI.Column_GUI;
+
+import Business_Logic.Card;
+import Business_Logic.Column;
+import GUI.Card_GUI.*;
+import GUI.Transfer.DropHandler;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import javax.swing.JPanel;
+
+public class DropPane extends JPanel {
+
+    DropTarget dropTarget;
+    DropHandler dropHandler;
+
+    public DropPane() {
+        setBackground(Color.BLUE);
+    }
+
+    @Override
+    public void addNotify() {
+        
+        super.addNotify(); //To change body of generated methods, choose Tools | Templates.
+        dropHandler = new DropHandler();
+        dropTarget = new DropTarget(this, DnDConstants.ACTION_MOVE, dropHandler, true);
+    }
+
+    @Override
+    public void removeNotify() {
+        super.removeNotify(); //To change body of generated methods, choose Tools | Templates.
+        dropTarget.removeDropTargetListener(dropHandler);
+    }
+
+}
